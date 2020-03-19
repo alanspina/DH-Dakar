e08b0be2a1926b75c12f2944ccf9a6ce5d005f06
alan <alanspina@gmail.com>
2020-03-18 21:32:00 -0300

    2. Modelar la clase Carrera que tiene los siguientes atributos:
    2. Modelar la clase Carrera que tiene los siguientes atributos:
    ○ Distancia
    ○ PremioEnDolares
    ○ Nombre
    ○ CantidadDeVehiculosPermitidos
    ○ Lista de Vehiculos

---
+++ src/com/company/Carrera.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.List;
+
+public class Carrera {
+    private Integer distancia;
+    private Integer premioEnDolares;
+    private Integer nombre;
+    private Integer cantidadDeVehiculosPermitidos;
+    private List<Vehiculo> listaDeVehículos;
+
+    
+}
